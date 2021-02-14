88d5642559c24c9a190a9975bfc290732e0cb72a
unallsukru <unallsukru@gmail.com>
2021-02-14 14:01:52 +0300

    initial commit :100:
    -auth page
    -some changes on _app.tsx and container.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import { AppProps } from "next/app"
 import { ChakraProvider, CSSReset, Flex } from "@chakra-ui/react"
-import Navbar from "../components/Navbar"
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <ChakraProvider>
       <CSSReset />
       <Flex width="100%" height="100%" minH="100vh" flexDirection="column">
-        <Navbar />
         <Component {...pageProps} />
       </Flex>
     </ChakraProvider>
