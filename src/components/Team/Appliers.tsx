3a86a7198f91266c0704711e52a63e2e77c49dc4
Kerem Esen <kerem.esen.01@hotmail.com>
2021-02-26 21:32:04 +0300

    team great again !

--- src/components/Team/Appliers.tsx
+++ src/components/Team/Appliers.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Appliers: React.FC<Props> = ({ appliersName, appliersDesc }) => {
   return (
-    <Flex w="581px" h="100%" bgColor="#95E3F8" borderRadius="13px" ml={6} mb={4} direction="row">
+    <Flex w="581px" h="auto" bgColor="#95E3F8" borderRadius="13px" ml={6} mb={4} direction="row">
       <Flex
         w="73px"
         h="73px"
@@ -20,35 +20,40 @@ const Appliers: React.FC<Props> = ({ appliersName, appliersDesc }) => {
       >
         <Avatar size="lg" name="kerem esen" src="https://bit.ly/dan-abramov" cursor="pointer" />
       </Flex>
-      <Flex direction="column" align="flex-start" w="60%" h="100%">
+      <Flex direction="column" align="flex-start" w="80%" h="100%">
         <Text fontWeight="500" textColor="#003848">
           {appliersName}
         </Text>
         <Text textColor="#003848" noOfLines={6}>
           {appliersDesc}
         </Text>
-      </Flex>
-      <Flex
-        align="center"
-        justify="space-between"
-        w="100px"
-        h="auto"
-        mt={5}
-        ml={2}
-        mb={2}
-        direction="column"
-      >
-        <Image src="/cross.svg" cursor="pointer" />
-        <Button
-          w="116px"
-          h="29px"
-          bgColor="#20D79E"
-          borderRadius="47px"
-          textColor="#fff"
-          _hover={{ bgColor: "#00FFAA" }}
-        >
-          Accept
-        </Button>
+        <Flex h="auto" w="100%" justify="flex-end" align="flex-end" direction="row">
+          <Button
+            bgColor="#D72020"
+            w="76px"
+            h="29px"
+            borderRadius="47px"
+            textColor="#fff"
+            mb={2}
+            fontSize="sm"
+            _hover={{bgColor: "#ff352e"}}
+          >
+            SİL
+          </Button>
+          <Button
+            w="116px"
+            h="29px"
+            bgColor="#20D79E"
+            borderRadius="47px"
+            textColor="#fff"
+            _hover={{ bgColor: "#00FFAA" }}
+            mb={2}
+            mx={2}
+            fontSize="sm"
+          >
+            ONAYLA
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   )
